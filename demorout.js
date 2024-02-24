cf510b122042bde65bfeedfef4dd5a153ccf8e83
yared <fdessalew@gmail.com>
2024-02-24 21:39:15 +0300

    add routing

---
+++ demorout.js
@@ -0,0 +1,15 @@
+// get  near orders
+'/orders/lat:1111/lon:1234/'
+// get single order 
+'/orders/order-code'
+//get user and driver  by id
+'/api/users/user-id:1222'
+'/api/driver/userid'
+//get near driver
+'/api/driver/lat:122/lan:122/'
+//track orders single order
+'/api/messages/order-id'
+// get all near orders
+'/api/messages/lat:123/lon:12122'
+// get my orders
+'/api/messages/userid'
